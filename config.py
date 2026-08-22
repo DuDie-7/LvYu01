7308b8b7f23a05111cd667e266213d6f767f78e0
DuDie-7 <2590224571@qq.com>
2026-08-22 15:56:29 +0800

    Update config.py
    替换掉原来的硬编码本地数据库地址

--- config.py
+++ config.py
@@ -2,6 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-change-this'
-    # 修改为你的 MySQL 用户名和密码，数据库名为 message_board
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@localhost:3306/message_board'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://root:123456@localhost:3306/message_board'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
